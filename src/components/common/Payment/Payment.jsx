8aa5a52bbf14d62f8845278074e59a3e42d75754
Anthon_M <xll2005@tut.by>
2020-02-12 12:30:28 +0300

    feat: add Payment component

---
+++ src/components/common/Payment/Payment.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Spinner from '../../Spinner';
+
+import './Payment.scss';
+
+const Payment = ({ payment, isCalculate }) => (
+  <div className="payment">
+    {
+      isCalculate ? (
+        <p className="payment__text">
+          $
+          {payment}
+          <span className="payment__term">/mo</span>
+        </p>
+      ) : <Spinner />
+    }
+  </div>
+);
+
+Payment.propTypes = {
+  isCalculate: PropTypes.bool.isRequired,
+  payment: PropTypes.number.isRequired,
+};
+
+export default Payment;
