05dc0d9c71007dc325b37c4d5425ca02b3566150
Anthon_M <xll2005@tut.by>
2020-02-12 12:35:11 +0300

    feat: add Spinner component

---
+++ src/components/common/Spinner/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Spinner.scss';
+
+const Spinner = () => (
+  <div className="loadingio-spinner-rolling-maveppxfqpo">
+    <div className="ldio-4e8kd7u4r4t">
+      <div />
+    </div>
+  </div>
+);
+
+export default Spinner;
